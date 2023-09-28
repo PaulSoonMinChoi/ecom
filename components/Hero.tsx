be4a15de8be04762d1c1044bc60014f519963252
paulsoonminchoi <paulsoonminchoi@gmail.com>
2023-09-28 15:02:37 -0700

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                FLATBLACK
+            </h1>
+            <p className='hero__subtitle'>
+                Integrated Studio + Manufacturing (Since 2010 - _________ )
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
